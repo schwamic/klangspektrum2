0623bb97a6c2061890effed6132a326d0b46122e
schwamic <ms@tresmo.de>
2018-06-08 21:33:26 +0200

    web: add routing-example to test server-settings

---
+++ packages/web/src/app/first/first.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FirstComponent } from './container/first.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [FirstComponent]
+})
+export class FirstModule { }
