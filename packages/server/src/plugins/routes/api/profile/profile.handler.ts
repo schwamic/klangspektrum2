853fde9eceeaeaf77c9d005015e4d79abca6c279
schwamic <ms@tresmo.de>
2018-06-09 10:38:08 +0200

    server: add logging and clean code

--- packages/server/src/plugins/routes/api/profile/profile.handler.ts
+++ packages/server/src/plugins/routes/api/profile/profile.handler.ts
@@ -1,6 +1,10 @@
 import * as Hapi from 'hapi'
 import * as qs from 'qs'
 
+/**
+ * loads user-profile-data
+ * @type {{index: (request: Request, h: ResponseToolkit) => Promise<ResponseObject>}}
+ */
 export const ProfileHandler = {
   index: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
     const access_token = request.query['access_token']
