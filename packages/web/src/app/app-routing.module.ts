16f321efc847e8e10d85a64194d137a30f4bcbf0
schwamic <ms@tresmo.de>
2018-07-26 07:41:38 +0200

    refactor(web): cli test

--- packages/web/src/app/app-routing.module.ts
+++ packages/web/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import {RouterModule, Routes} from '@angular/router'
+import {NgModule} from '@angular/core'
+
 
 const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'}
@@ -9,5 +10,6 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {
 }
