384b9c2e955ce0f8658bdd95ea797b0712a7d2a3
schwamic <ms@tresmo.de>
2018-07-26 07:33:18 +0200

    fix(web): tests

--- packages/web/src/app/app-routing.module.ts
+++ packages/web/src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { FirstComponent } from './first/components/first.component'
-import { SecondComponent } from './second/components/second.component'
 
 const routes: Routes = [
-  {path: 'first', component: FirstComponent},
-  {path: 'second', component: SecondComponent},
-  // {path: '', redirectTo: 'main', pathMatch: 'full'},
-  {path: '**', redirectTo: 'first'}
+  {path: '', redirectTo: 'main', pathMatch: 'full'}
 ]
 
 @NgModule({
