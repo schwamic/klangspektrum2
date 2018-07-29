65b98f20ec0af4771c4ba17d7d8483464bd9b7d5
schwamic <ms@tresmo.de>
2018-07-29 14:10:00 +0200

    chore: secure env variables

--- packages/web/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  test1: 'test',
-  test2: process.env.HOME
-};
--- packages/web/src/environments/environment.ts
+++ packages/web/src/environments/environment.ts
@@ -1,15 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientId: "6dcb1e5d914e4492b172b130e8912b6f",
+  redirectUri: "http://localhost:8020/callback" 
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
