4df70c7ac2dc973d7955a073171cd3ab8a3fb5ac
schwamic <ms@tresmo.de>
2018-07-31 08:00:06 +0200

    feat(api-service): init login-logic

---
+++ packages/web/src/app/core/services/api.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core'
+import {HttpClient, HttpParams} from "@angular/common/http"
+import {environment} from "@env/environment"
+import * as uuid from 'uuid'
+import * as qs from 'qs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  state: string
+
+  constructor(private http: HttpClient) { }
+
+  login(){
+    this.state = uuid()
+    localStorage.setItem('xsrf-token', this.state)
+    const params = {
+        response_type: 'token',
+        client_id: environment.clientId,
+        redirect_uri: environment.redirectUri,
+        scope: 'user-read-private user-read-email playlist-read-private playlist-read-collaborative user-library-read user-top-read',
+        state: this.state,
+        show_dialog: 'true'
+    }
+    const redirectUrl = `https://accounts.spotify.com/authorize?${qs.stringify(params)}`
+    window.location.href = redirectUrl
+  }
+
+  // todo add callback -> guard -> load data
+  // todo add interceptor to set header
+  profile(access_token){
+    const options = {
+      params: new HttpParams()
+        .set('headers', qs.stringify({'Authorization': `Bearer ${access_token}`}))
+        .set('json', 'true')
+    }
+    this.http.get('https://api.spotify.com/v1/me', options)
+  }
+}
--- packages/web/src/app/home/components/landing-page/landing-page.component.ts
+++ packages/web/src/app/home/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '@app/core/services/api.service'
 
 @Component({
   selector: 'ks-landing-page',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
   }
 
+  login(){
+    this.api.login()
+  }
 }
