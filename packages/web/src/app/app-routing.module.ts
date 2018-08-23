eec5eeaafc7362fa2f5bb215f6c6b9cebac798fa
schwamic <ms@tresmo.de>
2018-08-23 08:13:06 +0200

    feat(auth-interceptor): testing errorhandling in interceptor...

--- packages/web/src/app/app-routing.module.ts
+++ packages/web/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import {RouterModule, Routes} from '@angular/router'
 import {NgModule} from '@angular/core'
 import {NotFoundComponent} from '@app/core/components/not-found/not-found.component'
-import {MetaGuard} from "@app/core/guards/meta.guard";
-import {ErrorComponent} from "@app/shared/components/error/error.component";
+import {MetaGuard} from '@app/core/guards/meta.guard'
+import {ErrorComponent} from '@app/shared/components/error/error.component'
 
 
 const routes: Routes = [
--- packages/web/src/app/core/http-interceptors/auth.interceptor.ts
+++ packages/web/src/app/core/http-interceptors/auth.interceptor.ts
@@ -1,20 +1,31 @@
 import {Injectable} from '@angular/core'
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http'
-import {Observable} from 'rxjs'
+import {empty, Observable, of, throwError} from 'rxjs'
 import {Store} from "@ngrx/store";
 import * as fromStore from "../store"
-import {first, map, switchMap, tap} from "rxjs/operators";
+import {catchError, first, map, switchMap} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private store: Store<fromStore.State>) {}
+  constructor(private store: Store<fromStore.State>, private router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return this.store.select(fromStore.selectAccessToken).pipe(
       first(),
       map(token => req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })),
-      switchMap(req => next.handle(req))
+      switchMap(req => next.handle(req)
+        .pipe(
+          catchError((err, source) => {
+            if (err.status  == 401 || err.status == 403) {
+              this.router.navigate(['/home', {needsLogin: true}]) //todo: not working -> errorhandling in guards
+              return empty()
+            } else {
+              return throwError(err)
+            }
+          })
+        ))
     )
   }
 }
