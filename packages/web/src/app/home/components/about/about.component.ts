421e5735a0de48d0c375f2431c86903eb6862811
schwamic <ms@tresmo.de>
2018-09-03 08:51:51 +0200

    feat(web): add basic styling

--- packages/web/src/app/home/components/about/about.component.ts
+++ packages/web/src/app/home/components/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ks-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
