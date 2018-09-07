c98bc3610ee5f250759f6bc8fdece6d9edf97a56
schwamic <ms@tresmo.de>
2018-09-07 09:08:16 +0200

    feat(web): add basic layout to filter

---
+++ packages/web/src/app/visualization/components/filter/filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ks-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss']
+})
+export class FilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
