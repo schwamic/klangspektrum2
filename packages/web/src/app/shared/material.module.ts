635e2dc93d74ea6377e9409de4ff5097fe2092e1
schwamic <ms@tresmo.de>
2018-09-08 11:53:44 +0200

    feat(web): improve sidenav

--- packages/web/src/app/shared/material.module.ts
+++ packages/web/src/app/shared/material.module.ts
@@ -23,7 +23,8 @@ import {
   MatTabsModule,
   MatSlideToggleModule,
   MatPaginatorModule,
-  MatCheckboxModule
+  MatCheckboxModule,
+  MatTreeModule
 } from '@angular/material'
 
 @NgModule({
@@ -51,7 +52,8 @@ import {
     MatTabsModule,
     MatSlideToggleModule,
     MatPaginatorModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatTreeModule
   ],
   exports: [
     MatButtonModule,
@@ -77,7 +79,8 @@ import {
     MatTabsModule,
     MatSlideToggleModule,
     MatPaginatorModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatTreeModule
   ],
   declarations: []
 })
