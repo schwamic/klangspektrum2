8f9343972221227e3c3d0800d42dd27461ed9680
schwamic <ms@tresmo.de>
2018-12-26 12:23:00 +0100

    chore: init components

--- packages/web/src/app/music/containers/playlist/playlist.component.ts
+++ packages/web/src/app/music/containers/playlist/playlist.component.ts
@@ -3,7 +3,7 @@ import * as fromRoot from '../../../core/store'
 import { Component, OnInit } from '@angular/core'
 
 import { Store } from '@ngrx/store'
-import { map } from 'rxjs/operators';
+import { map } from 'rxjs/operators'
 import { selectAllTracks } from '../../../core/store/index'
 
 @Component({
@@ -12,8 +12,7 @@ import { selectAllTracks } from '../../../core/store/index'
   styleUrls: ['./playlist.component.scss']
 })
 export class PlaylistComponent implements OnInit {
-
-  pagination = 10
+  pagination = 30
 
   tracks$ = this.store.select(selectAllTracks).pipe(map(tracks => tracks.slice(0, this.pagination)))
   constructor(private store: Store<fromRoot.State>) {}
--- packages/web/src/app/music/containers/visualization/visualization.component.ts
+++ packages/web/src/app/music/containers/visualization/visualization.component.ts
@@ -10,14 +10,18 @@ export class VisualizationComponent {
     responsive: true
   }
 
-  chartData = [
-    { data: [330, 600, 260, 700], label: 'All' },
-    { data: [120, 455, 100, 340], label: 'Selection' },
+  chartData = [{ data: [330, 600, 260, 700, 330, 600, 260, 700, 30, 400], label: 'All' }]
+
+  chartLabels = [
+    '0 - 0.1',
+    '0.1 - 0.2',
+    '0.2 - 0.3',
+    '0.3 - 0.4',
+    '0.4 - 0.5',
+    '0.5 - 0.6',
+    '0.6 - 0.7',
+    '0.7 - 0.8',
+    '0.8 - 0.9',
+    '0.9 - 1'
   ]
-
-  chartLabels = ['January', 'February', 'Mars', 'April']
-
-  onChartClick(event) {
-    console.log(event)
-  }
 }
