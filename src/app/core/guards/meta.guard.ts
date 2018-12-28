8017ac7aeb6dc0b8ff0f937be70a31c64c036490
schwamic <ms@tresmo.de>
2018-12-28 18:40:29 +0100

    chore: redirect

--- src/app/core/guards/meta.guard.ts
+++ src/app/core/guards/meta.guard.ts
@@ -21,7 +21,7 @@ export class MetaGuard implements CanActivate {
         const valid = this.isValid(meta)
         if (valid) {
           this.store.dispatch(new AddMeta({ ...meta }))
-          this.router.navigate(['player'])
+          this.router.navigate(['visualizer'])
         }
         return !valid
       }),
