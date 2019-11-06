1bb79b1b7312abe5cdf04cbb50ffdfc75c1bfca1
Michael Schwarz <18497277+schwamic@users.noreply.github.com>
2019-11-07 00:28:45 +0100

    chore: update node and yarn (#46)
    * feat: update dependencies
    
    * chore: update dependencies
    
    * linting
    
    * update node and yarn
    
    * update node version circleci
    
    * fix: typo and padding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core'
 import { StateService } from './core/services/state.service'
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <p *ngIf="(loading$ | async)" style="margin: 40px 32px;">...loading</p>
+	selector: 'app-root',
+	template: `
+    <p *ngIf="(loading$ | async)" style="margin: 40px 32px;">loading...</p>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
-  loading$ = this.stateService.getLoadingState()
-  constructor(private stateService: StateService) {}
+	loading$ = this.stateService.getLoadingState()
+	constructor(private stateService: StateService) {}
 }
