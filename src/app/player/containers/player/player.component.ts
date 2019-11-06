19179818dbac7c535d786ee1449e66b104b4a4d7
Michael Schwarz <18497277+schwamic@users.noreply.github.com>
2019-11-06 23:36:22 +0100

    Chore/linting (#45)
    * feat: update dependencies
    
    * chore: update dependencies
    
    * linting

--- src/app/player/containers/player/player.component.ts
+++ src/app/player/containers/player/player.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import * as fromRoot from '../../../core/store';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
-import { combineLatest, Subscription } from 'rxjs';
-import { StateService } from '@app/core/services/state.service';
-import { SelectionModel } from '@angular/cdk/collections';
-import { map, first } from 'rxjs/operators';
-import { Throttle } from 'lodash-decorators/throttle';
-import { PlayerService } from '@app/core/services/player.service';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core'
+import { Store, select } from '@ngrx/store'
+import * as fromRoot from '../../../core/store'
+import { MatPaginator, MatTableDataSource } from '@angular/material'
+import { combineLatest, Subscription } from 'rxjs'
+import { StateService } from '@app/core/services/state.service'
+import { SelectionModel } from '@angular/cdk/collections'
+import { map, first } from 'rxjs/operators'
+import { Throttle } from 'lodash-decorators/throttle'
+import { PlayerService } from '@app/core/services/player.service'
 
 @Component({
 	selector: 'app-player',
@@ -16,13 +16,13 @@ import { PlayerService } from '@app/core/services/player.service';
 })
 export class PlayerComponent implements OnInit, OnDestroy {
 	@ViewChild(MatPaginator, { static: true })
-	paginator: MatPaginator;
-	dataSource;
-	selection = new SelectionModel(true, []);
-	sub = new Subscription();
-	track$ = this.playerService.getCurrentTrack();
-	shuffleState$ = this.playerService.getShuffleState();
-	profile$ = this.store.pipe(select(fromRoot.selectProfile));
+	paginator: MatPaginator
+	dataSource
+	selection = new SelectionModel(true, [])
+	sub = new Subscription()
+	track$ = this.playerService.getCurrentTrack()
+	shuffleState$ = this.playerService.getShuffleState()
+	profile$ = this.store.pipe(select(fromRoot.selectProfile))
 
 	constructor(
 		private store: Store<fromRoot.State>,
@@ -55,54 +55,54 @@ export class PlayerComponent implements OnInit, OnDestroy {
 									track.features.speechiness <= filter.speechiness[1]) &&
 								(track.features.valence >= filter.valence[0] &&
 									track.features.valence <= filter.valence[1])
-							);
-						});
+							)
+						})
 					})
 				)
 				.subscribe((tracks) => {
-					this.dataSource = new MatTableDataSource(tracks);
-					this.dataSource.paginator = this.paginator;
+					this.dataSource = new MatTableDataSource(tracks)
+					this.dataSource.paginator = this.paginator
 				})
-		);
+		)
 	}
 
 	ngOnDestroy() {
-		this.sub.unsubscribe();
+		this.sub.unsubscribe()
 	}
 	/**
-   * Select item in table and set new track
-   * @param row
-   */
+  * Select item in table and set new track
+  * @param row
+  */
 	@Throttle(300)
 	select(row) {
-		this.selection.clear();
-		this.selection.select(row);
-		this.playerService.setCurrentTrack(row);
-		this.playerService.play(row.uri).pipe(first()).subscribe();
+		this.selection.clear()
+		this.selection.select(row)
+		this.playerService.setCurrentTrack(row)
+		this.playerService.play(row.uri).pipe(first()).subscribe()
 	}
 
 	selectData(state: string) {
-		const selected = this.selection.selected[0];
-		const index = this.dataSource.data.indexOf(selected);
+		const selected = this.selection.selected[0]
+		const index = this.dataSource.data.indexOf(selected)
 		this.playerService.getShuffleState().pipe(first()).subscribe((shuffleState) => {
 			if (shuffleState) {
-				const random = this.getRandomIntInclusive(0, this.dataSource.data.length);
-				this.select(this.dataSource.data[random]);
+				const random = this.getRandomIntInclusive(0, this.dataSource.data.length)
+				this.select(this.dataSource.data[random])
 			} else {
-				let newIndex = state === 'next' ? index + 1 : index - 1;
-				newIndex = newIndex < 0 ? 0 : newIndex;
-				newIndex = newIndex > this.dataSource.data.length ? this.dataSource.data.length : newIndex;
-				this.select(this.dataSource.data[newIndex]);
+				let newIndex = state === 'next' ? index + 1 : index - 1
+				newIndex = newIndex < 0 ? 0 : newIndex
+				newIndex = newIndex > this.dataSource.data.length ? this.dataSource.data.length : newIndex
+				this.select(this.dataSource.data[newIndex])
 			}
-		});
+		})
 	}
 
 	/**
-   * The maximum and minimum are inclusive
-   */
+  * The maximum and minimum are inclusive
+  */
 	getRandomIntInclusive(min, max) {
-		min = Math.ceil(min);
-		max = Math.floor(max);
-		return Math.floor(Math.random() * (max - min + 1)) + min;
+		min = Math.ceil(min)
+		max = Math.floor(max)
+		return Math.floor(Math.random() * (max - min + 1)) + min
 	}
 }
--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -6,7 +6,7 @@ import { SharedModule } from '@app/shared/shared.module'
 import { FilterComponent } from './components/filter/filter.component'
 import { ReactiveFormsModule } from '@angular/forms'
 import { NouisliderModule } from 'ng2-nouislider'
-import { PlayerControllerComponent } from './components/player-controller/player-controller.component';
+import { PlayerControllerComponent } from './components/player-controller/player-controller.component'
 import { TrackComponent } from './components/track/track.component'
 
 @NgModule({
