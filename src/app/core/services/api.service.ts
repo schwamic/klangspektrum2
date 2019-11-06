9a0460c4e05113d99cee550a8099a921d8c5e131
Michael Schwarz <18497277+schwamic@users.noreply.github.com>
2019-11-06 23:19:43 +0100

    Chore/update dependencies (#43)
    * feat: update dependencies
    
    * chore: update dependencies

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,114 +1,96 @@
-import * as chunk from 'lodash/chunk'
-import * as flatten from 'lodash/flatten'
-import * as fromStore from '../store'
-import * as join from 'lodash/join'
-import * as qs from 'qs'
-import * as uuid from 'uuid'
-import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import { Store } from '@ngrx/store'
-import { Track } from '@app/shared/models/track.model'
-import { TrackService } from '@app/core/services/track.service'
-import { environment } from '@env/environment'
-import { Observable, forkJoin, from, throwError, empty } from 'rxjs'
-import {
-  catchError,
-  first,
-  map,
-  mergeMap,
-  scan,
-  switchMap,
-  tap,
-  retryWhen,
-  retry,
-  delay
-} from 'rxjs/operators'
+import * as chunk from 'lodash/chunk';
+import * as flatten from 'lodash/flatten';
+import * as fromStore from '../store';
+import * as join from 'lodash/join';
+import * as qs from 'qs';
+import * as uuid from 'uuid';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Track } from '@app/shared/models/track.model';
+import { TrackService } from '@app/core/services/track.service';
+import { environment } from '@env/environment';
+import { Observable, forkJoin, from, throwError, empty } from 'rxjs';
+import { catchError, first, map, mergeMap, scan, switchMap, tap, retryWhen, retry, delay } from 'rxjs/operators';
 
 // TODO
 // + error-handling + types
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ApiService {
-  constructor(
-    private http: HttpClient,
-    private trackService: TrackService,
-    private store: Store<fromStore.State>
-  ) {}
+	constructor(private http: HttpClient, private trackService: TrackService, private store: Store<fromStore.State>) {}
 
-  login(showDialog = true): void {
-    const state = uuid()
-    localStorage.setItem('xsrf-token', state)
-    const params = {
-      response_type: 'token',
-      client_id: environment.clientId,
-      redirect_uri: environment.redirectUri,
-      scope:
-        'streaming user-read-private user-read-birthdate user-read-email playlist-read-private playlist-read-collaborative user-library-read user-top-read',
-      state,
-      show_dialog: `${showDialog}`
-    }
-    const redirectUrl = `https://accounts.spotify.com/authorize?${qs.stringify(params)}`
-    window.location.href = redirectUrl
-  }
+	login(showDialog = true): void {
+		const state = uuid();
+		localStorage.setItem('xsrf-token', state);
+		const params = {
+			response_type: 'token',
+			client_id: environment.clientId,
+			redirect_uri: environment.redirectUri,
+			scope:
+				'streaming user-read-private user-read-birthdate user-read-email playlist-read-private playlist-read-collaborative user-library-read user-top-read',
+			state,
+			show_dialog: `${showDialog}`
+		};
+		const redirectUrl = `https://accounts.spotify.com/authorize?${qs.stringify(params)}`;
+		window.location.href = redirectUrl;
+	}
 
-  profile(): Observable<any> {
-    return this.http.get('https://api.spotify.com/v1/me').pipe(
-      retry(3),
-      catchError(error => throwError(error))
-    )
-  }
+	profile(): Observable<any> {
+		return this.http.get('https://api.spotify.com/v1/me').pipe(retry(3), catchError((error) => throwError(error)));
+	}
 
-  tracks(): Observable<Track[]> {
-    return this.trackService.getAllTracks().pipe(catchError(error => throwError(error)))
-  }
+	tracks(): Observable<Track[]> {
+		return this.trackService.getAllTracks().pipe(catchError((error) => throwError(error)));
+	}
 
-  features(): Observable<any> {
-    return forkJoin(
-      this.store.select(fromStore.selectTrack).pipe(
-        first(),
-        switchMap(tracks =>
-          from(chunk(tracks.ids, 100)).pipe(
-            mergeMap(
-              trackIds =>
-                this.getData(
-                  `https://api.spotify.com/v1/audio-features?ids=${join(trackIds, ',')}`
-                ),
-              null,
-              4
-            ),
-            scan((acc, features) => [...acc, ...features], [])
-          )
-        )
-      )
-    ).pipe(
-      map(features => flatten(features[0].map(a => a.audio_features)).filter(feature => !!feature))
-    )
-  }
+	features(): Observable<any> {
+		return forkJoin(
+			this.store
+				.select(fromStore.selectTrack)
+				.pipe(
+					first(),
+					switchMap((tracks) =>
+						from(chunk(tracks.ids, 100)).pipe(
+							mergeMap(
+								(trackIds) =>
+									this.getData(
+										`https://api.spotify.com/v1/audio-features?ids=${join(trackIds, ',')}`
+									),
+								null,
+								4
+							),
+							scan((acc, features) => [ ...acc, ...(features as Array<any>) ], [])
+						)
+					)
+				)
+		).pipe(map((features) => flatten(features[0].map((a) => a.audio_features)).filter((feature) => !!feature)));
+	}
 
-  /**
+	/**
    * helper for simple request
    * @param url
    */
-  getData(url) {
-    return this.http.get(url).pipe(
-      retryWhen(errors =>
-        errors.pipe(
-          delay(1000),
-          tap(error => {
-            if (error.status !== 429) {
-              throw error
-            }
-          })
-        )
-      ),
-      first(),
-      catchError(error => {
-        /* tslint:disable no-console */
-        console.log('hanlde error', error)
-        return empty()
-      })
-    )
-  }
+	getData(url) {
+		return this.http.get(url).pipe(
+			retryWhen((errors) =>
+				errors.pipe(
+					delay(1000),
+					tap((error) => {
+						if (error.status !== 429) {
+							throw error;
+						}
+					})
+				)
+			),
+			first(),
+			catchError((error) => {
+				/* tslint:disable no-console */
+				console.log('hanlde error', error);
+				return empty();
+			})
+		);
+	}
 }
--- src/app/player/containers/player/player.component.ts
+++ src/app/player/containers/player/player.component.ts
@@ -1,113 +1,108 @@
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core'
-import { Store, select } from '@ngrx/store'
-import * as fromRoot from '../../../core/store'
-import { MatPaginator, MatTableDataSource } from '@angular/material'
-import { combineLatest, Subscription } from 'rxjs'
-import { StateService } from '@app/core/services/state.service'
-import { SelectionModel } from '@angular/cdk/collections'
-import { map, first } from 'rxjs/operators'
-import { Throttle } from 'lodash-decorators/throttle'
-import { PlayerService } from '@app/core/services/player.service'
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import * as fromRoot from '../../../core/store';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { combineLatest, Subscription } from 'rxjs';
+import { StateService } from '@app/core/services/state.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { map, first } from 'rxjs/operators';
+import { Throttle } from 'lodash-decorators/throttle';
+import { PlayerService } from '@app/core/services/player.service';
 
 @Component({
-  selector: 'app-player',
-  templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+	selector: 'app-player',
+	templateUrl: './player.component.html',
+	styleUrls: [ './player.component.scss' ]
 })
 export class PlayerComponent implements OnInit, OnDestroy {
-  @ViewChild(MatPaginator) paginator: MatPaginator
-  dataSource
-  selection = new SelectionModel(true, [])
-  sub = new Subscription()
-  track$ = this.playerService.getCurrentTrack()
-  shuffleState$ = this.playerService.getShuffleState()
-  profile$ = this.store.pipe(select(fromRoot.selectProfile))
+	@ViewChild(MatPaginator, { static: true })
+	paginator: MatPaginator;
+	dataSource;
+	selection = new SelectionModel(true, []);
+	sub = new Subscription();
+	track$ = this.playerService.getCurrentTrack();
+	shuffleState$ = this.playerService.getShuffleState();
+	profile$ = this.store.pipe(select(fromRoot.selectProfile));
 
-  constructor(
-    private store: Store<fromRoot.State>,
-    private stateService: StateService,
-    private playerService: PlayerService
-  ) {}
+	constructor(
+		private store: Store<fromRoot.State>,
+		private stateService: StateService,
+		private playerService: PlayerService
+	) {}
 
-  ngOnInit() {
-    this.sub.add(
-      combineLatest(
-        this.store.pipe(select(fromRoot.selectTracksWithFeatures)),
-        this.stateService.getFilterSettings()
-      )
-        .pipe(
-          map(([tracks, filter]) => {
-            return tracks.filter(track => {
-              return (
-                !!track.features &&
-                (track.features.acousticness >= filter.acousticness[0] &&
-                  track.features.acousticness <= filter.acousticness[1]) &&
-                (track.features.danceability >= filter.danceability[0] &&
-                  track.features.danceability <= filter.danceability[1]) &&
-                (track.features.energy >= filter.energy[0] &&
-                  track.features.energy <= filter.energy[1]) &&
-                (track.features.instrumentalness >= filter.instrumentalness[0] &&
-                  track.features.instrumentalness <= filter.instrumentalness[1]) &&
-                (track.features.liveness >= filter.liveness[0] &&
-                  track.features.liveness <= filter.liveness[1]) &&
-                (track.features.speechiness >= filter.speechiness[0] &&
-                  track.features.speechiness <= filter.speechiness[1]) &&
-                (track.features.valence >= filter.valence[0] &&
-                  track.features.valence <= filter.valence[1])
-              )
-            })
-          })
-        )
-        .subscribe(tracks => {
-          this.dataSource = new MatTableDataSource(tracks)
-          this.dataSource.paginator = this.paginator
-        })
-    )
-  }
+	ngOnInit() {
+		this.sub.add(
+			combineLatest(
+				this.store.pipe(select(fromRoot.selectTracksWithFeatures)),
+				this.stateService.getFilterSettings()
+			)
+				.pipe(
+					map(([ tracks, filter ]) => {
+						return tracks.filter((track) => {
+							return (
+								!!track.features &&
+								(track.features.acousticness >= filter.acousticness[0] &&
+									track.features.acousticness <= filter.acousticness[1]) &&
+								(track.features.danceability >= filter.danceability[0] &&
+									track.features.danceability <= filter.danceability[1]) &&
+								(track.features.energy >= filter.energy[0] &&
+									track.features.energy <= filter.energy[1]) &&
+								(track.features.instrumentalness >= filter.instrumentalness[0] &&
+									track.features.instrumentalness <= filter.instrumentalness[1]) &&
+								(track.features.liveness >= filter.liveness[0] &&
+									track.features.liveness <= filter.liveness[1]) &&
+								(track.features.speechiness >= filter.speechiness[0] &&
+									track.features.speechiness <= filter.speechiness[1]) &&
+								(track.features.valence >= filter.valence[0] &&
+									track.features.valence <= filter.valence[1])
+							);
+						});
+					})
+				)
+				.subscribe((tracks) => {
+					this.dataSource = new MatTableDataSource(tracks);
+					this.dataSource.paginator = this.paginator;
+				})
+		);
+	}
 
-  ngOnDestroy() {
-    this.sub.unsubscribe()
-  }
-  /**
+	ngOnDestroy() {
+		this.sub.unsubscribe();
+	}
+	/**
    * Select item in table and set new track
    * @param row
    */
-  @Throttle(300)
-  select(row) {
-    this.selection.clear()
-    this.selection.select(row)
-    this.playerService.setCurrentTrack(row)
-    this.playerService
-      .play(row.uri)
-      .pipe(first())
-      .subscribe()
-  }
+	@Throttle(300)
+	select(row) {
+		this.selection.clear();
+		this.selection.select(row);
+		this.playerService.setCurrentTrack(row);
+		this.playerService.play(row.uri).pipe(first()).subscribe();
+	}
 
-  selectData(state: string) {
-    const selected = this.selection.selected[0]
-    const index = this.dataSource.data.indexOf(selected)
-    this.playerService
-      .getShuffleState()
-      .pipe(first())
-      .subscribe(shuffleState => {
-        if (shuffleState) {
-          const random = this.getRandomIntInclusive(0, this.dataSource.data.length)
-          this.select(this.dataSource.data[random])
-        } else {
-          let newIndex = state === 'next' ? index + 1 : index - 1
-          newIndex = newIndex < 0 ? 0 : newIndex
-          newIndex = newIndex > this.dataSource.data.length ? this.dataSource.data.length : newIndex
-          this.select(this.dataSource.data[newIndex])
-        }
-      })
-  }
+	selectData(state: string) {
+		const selected = this.selection.selected[0];
+		const index = this.dataSource.data.indexOf(selected);
+		this.playerService.getShuffleState().pipe(first()).subscribe((shuffleState) => {
+			if (shuffleState) {
+				const random = this.getRandomIntInclusive(0, this.dataSource.data.length);
+				this.select(this.dataSource.data[random]);
+			} else {
+				let newIndex = state === 'next' ? index + 1 : index - 1;
+				newIndex = newIndex < 0 ? 0 : newIndex;
+				newIndex = newIndex > this.dataSource.data.length ? this.dataSource.data.length : newIndex;
+				this.select(this.dataSource.data[newIndex]);
+			}
+		});
+	}
 
-  /**
+	/**
    * The maximum and minimum are inclusive
    */
-  getRandomIntInclusive(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
-  }
+	getRandomIntInclusive(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 }
