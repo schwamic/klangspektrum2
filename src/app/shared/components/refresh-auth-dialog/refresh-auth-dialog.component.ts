5f09589601c9fab33d1e53f250704413fbe6b45a
schwamic <ms@tresmo.de>
2020-03-03 09:39:38 +0100

    chore: update angular material

--- src/app/shared/components/refresh-auth-dialog/refresh-auth-dialog.component.ts
+++ src/app/shared/components/refresh-auth-dialog/refresh-auth-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core'
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Store, select } from '@ngrx/store'
 import * as fromCore from '@app/core/store'
 import { ApiService } from '@app/core/services/api.service'
