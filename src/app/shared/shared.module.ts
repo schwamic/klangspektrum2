9242666ddb83eba861f2a894b17045e6db7a254a
schwamic <ms@tresmo.de>
2020-07-15 22:15:56 +0200

    refactor: remove entryComponents

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ErrorComponent } from './components/error/error.component';
-import { MaterialModule } from './modules/material/material.module';
-import { RouterModule } from '@angular/router';
-import { TimePipe } from './pipes/time.pipe';
-import { RangePipe } from './pipes/range.pipe';
-import { RefreshAuthDialogComponent } from './components/refresh-auth-dialog/refresh-auth-dialog.component';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ErrorComponent } from './components/error/error.component'
+import { MaterialModule } from './modules/material/material.module'
+import { RouterModule } from '@angular/router'
+import { TimePipe } from './pipes/time.pipe'
+import { RangePipe } from './pipes/range.pipe'
+import { RefreshAuthDialogComponent } from './components/refresh-auth-dialog/refresh-auth-dialog.component'
 
 @NgModule({
-	imports: [ CommonModule, RouterModule, MaterialModule ],
-	declarations: [ ErrorComponent, RefreshAuthDialogComponent, TimePipe, RangePipe ],
-	exports: [ MaterialModule, RouterModule, ErrorComponent, RefreshAuthDialogComponent, TimePipe, RangePipe ]
-	// entryComponents: [RefreshAuthDialogComponent]
+  imports: [CommonModule, RouterModule, MaterialModule],
+  declarations: [ErrorComponent, RefreshAuthDialogComponent, TimePipe, RangePipe],
+  exports: [
+    MaterialModule,
+    RouterModule,
+    ErrorComponent,
+    RefreshAuthDialogComponent,
+    TimePipe,
+    RangePipe,
+  ],
 })
 export class SharedModule {}
